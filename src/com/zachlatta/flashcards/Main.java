61a82172ce8bae6b1df3cb0d7a27d7df8ccf2296
Zachary Latta <zchlatta@gmail.com>
2013-01-03 16:38:23 -0800

    Created main class.

---
+++ src/com/zachlatta/flashcards/Main.java
@@ -0,0 +1,14 @@
+package com.zachlatta.flashcards;
+
+/**
+ * Start of the application.
+ *
+ * @author Zachary Latta
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // start
+    }
+}
