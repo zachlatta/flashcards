d70d9274ce20cf9e5ba0c301ca006ce24314a96d
Zachary Latta <zchlatta@gmail.com>
2013-01-03 17:32:17 -0800

    Created essentials of file selection window

---
+++ src/com/zachlatta/flashcards/view/FileSelection.java
@@ -0,0 +1,13 @@
+package com.zachlatta.flashcards.view;
+
+import javax.swing.*;
+
+/**
+ * @author Zachary Latta
+ */
+public class FileSelection
+{
+    private JPanel panel1;
+    private JButton chooseAFileButton;
+    private JButton openFileButton;
+}
